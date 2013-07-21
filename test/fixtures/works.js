90f8f34f646defba5854815ee3a799f85d90141a
Ian Kelly <iandotkelly@gmail.com>
2013-07-21 18:38:24 -0400

    Improve logging

--- test/fixtures/works.js
+++ test/fixtures/works.js
@@ -6,6 +6,10 @@ function wait(fn) {
   setTimeout(fn, 200);
 }
 
+function forever() {
+  setTimeout(forever, 1000);
+}
+
 wait(function () {
   console.log('step 1');
   wait(function () {        
@@ -14,7 +18,8 @@ wait(function () {
       console.log('step 3');
       wait(function () {
         console.log('ready');
-		  process.send({cmd: 'ready'});
+		    process.send({cmd: 'ready'});
+        forever();
       });
     });
   });
